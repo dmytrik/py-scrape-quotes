ee1dbef1de069066b3d5da0a9bb38f0619535d13
dmytro <dmysamo@gmail.com>
2025-01-12 16:18:12 +0200

    solution

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,13 @@
-from dataclasses import dataclass
+import csv
+
+from dataclasses import dataclass, fields, astuple
+
+import httpx
+from bs4 import BeautifulSoup, Tag
+
+
+URL = "https://quotes.toscrape.com/"
+NUM_PAGES = 10
 
 
 @dataclass
@@ -8,8 +17,37 @@ class Quote:
     tags: list[str]
 
 
+QUOTE_FIELDS = [field.name for field in fields(Quote)]
+
+
+def parse_single_quote(quote: Tag) -> Quote:
+    text = quote.select_one(".text").text
+    author = quote.select_one(".author").text
+    tags_html = quote.select(".tag")
+    return Quote(
+        text=text,
+        author=author,
+        tags=[tag.text for tag in tags_html]
+    )
+
+
+def get_quotes() -> [Quote]:
+    all_quotes = []
+    with httpx.Client() as client:
+        for i in range(1, NUM_PAGES + 1):
+            content = client.get(f"{URL}page/{i}/").content
+            soup = BeautifulSoup(content, "html.parser")
+            quotes = soup.select(".quote")
+            all_quotes += [parse_single_quote(quote) for quote in quotes]
+    return all_quotes
+
+
 def main(output_csv_path: str) -> None:
-    pass
+    quotes = get_quotes()
+    with open(output_csv_path, "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(QUOTE_FIELDS)
+        writer.writerows([astuple(quote) for quote in quotes])
 
 
 if __name__ == "__main__":
